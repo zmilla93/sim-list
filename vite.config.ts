ae48f1969892e08ef6fddae96e2f1a1782cba6b8
zmilla93 <zmilla93@gmail.com>
2024-12-29 12:01:54 -0600

    updated base

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: 'https://zmilla93.github.io/sim-list/',
 });
